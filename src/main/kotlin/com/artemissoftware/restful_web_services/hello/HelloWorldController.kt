06149644cc6ac6a251d0145e3de94175d56ec22b
gustavo <gustavo.isaac@gmail.com>
2024-12-19 19:10:43 +0000

    Creating a Hello World REST API with Spring Boot

---
+++ src/main/kotlin/com/artemissoftware/restful_web_services/hello/HelloWorldController.kt
@@ -0,0 +1,14 @@
+package com.artemissoftware.restful_web_services.hello
+
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class HelloWorldController {
+
+    @GetMapping(path = ["/hello"])
+    fun helloWorld() = "Hello World"
+}
