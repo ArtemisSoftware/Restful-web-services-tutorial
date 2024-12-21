ee584ddeb61545a9c7db0d6bd9594f11e65f4c2b
gustavo <gustavo.isaac@gmail.com>
2024-12-21 12:08:38 +0000

    Creating User Entity and some test data

--- src/main/kotlin/com/artemissoftware/restful_web_services/socialmedia/User.kt
+++ src/main/kotlin/com/artemissoftware/restful_web_services/socialmedia/User.kt
@@ -1,11 +1,17 @@
 package com.artemissoftware.restful_web_services.socialmedia
 
 import com.fasterxml.jackson.annotation.JsonProperty
+import jakarta.annotation.Generated
+import jakarta.persistence.Entity
+import jakarta.persistence.Id
 import jakarta.validation.constraints.Past
 import jakarta.validation.constraints.Size
 import java.time.LocalDate
 
+@Entity(name = "user_details")
 data class User(
+    @Id
+    @Generated
     var id: Int,
     @field:Size(min = 2, message = "Must have a minimum of 2 characters")
     @JsonProperty("user_name")
